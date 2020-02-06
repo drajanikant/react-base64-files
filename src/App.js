3d87beef62a96744710ec17606bf947cdf089d15
Rajanikant <rajanikant.devmore@knorex.com>
2020-02-06 10:33:44 +0530

    Added code for the files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import FileBase64 from 'react-file-base64';
 
 function App() {
+
+  const [files, setFiles] = useState([]);
+
+  const setValues = (files) =>{
+    setFiles(files)
+  }
+
+  const getImages = () =>{
+    return files.map(file => (
+      <div>
+      <img src={file.base64} alt={file.name} />
+      <a href="#" onClick={()=>{downloadPDF(file.base64, file.name)}} >download</a>
+      </div>
+    ));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h2>Image to string</h2>
+      <hr />
+      {/* <input type="file" onChange={imgSelect}></input>
+      <img src={`data:text/html; charset=UTF-8;base64,${imageUrl}`}  alt="Some image" /> */}
+      <FileBase64
+        multiple={ true }
+        onDone={ setValues } />
+   
+   { getImages() }
     </div>
+
+
   );
 }
 
+/**
+ * Creates an anchor element `<a></a>` with
+ * the base64 pdf source and a filename with the
+ * HTML5 `download` attribute then clicks on it.
+ * @param  {string} base64 data
+ * @param  {string} file name 
+ * @return {void}     
+ */
+function downloadPDF(pdf, _fileName) {
+  const linkSource = pdf;
+  const downloadLink = document.createElement("a");
+  const fileName = _fileName;
+
+  downloadLink.href = linkSource;
+  downloadLink.download = fileName;
+  downloadLink.click();
+}
 export default App;
